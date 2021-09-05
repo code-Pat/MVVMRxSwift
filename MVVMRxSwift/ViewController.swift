916e315e5bcf8ea5af4ba3160dc4f063e624cf2a
Donggeun Lee <patlee997@gmail.com>
2021-09-05 20:26:53 +0900

    Just testing

--- MVVMRxSwift/ViewController.swift
+++ MVVMRxSwift/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Hi there
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
