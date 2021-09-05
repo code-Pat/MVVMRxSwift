34250a716d4ee44aecaba3c0d211cce22fabdbb6
Donggeun Lee <patlee997@gmail.com>
2021-09-05 22:38:18 +0900

    RootViewModel added

---
+++ MVVMRxSwift/ViewModels/RootViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewModel.swift
+//  MVVMRxSwift
+//
+//  Created by Donggeun Lee on 2021/09/05.
+//
+
+import Foundation
+import RxSwift
+
+final class RootViewModel {
+    let title = "My News"
+    
+    private let articleService: ArticleServiceProtocol
+    
+    init(articleService: ArticleServiceProtocol) {
+        self.articleService = articleService
+    }
+    
+    func fetchArticles() -> Observable<[Article]> {
+        articleService.fetchNews()
+    }
+}
